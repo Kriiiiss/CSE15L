99f9c9e1f18f2c01b9ecf89302588d7e8ba7d519
Kriiiiss <jic128@ucsd.edu>
2025-06-20 17:26:35 -0700

    Lab3

---
+++ CSE15L_lab3/ArrayTests.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ArrayTests {
+	@Test 
+	public void testReverseInPlace() {
+    int[] input1 = { 3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3 }, input1);
+	}
+
+
+  @Test
+  public void testReversed() {
+    int[] input1 = { };
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+
+
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = {1,2,3,4,5,6,7};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{7,6,5,4,3,2,1}, input1);
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,2,3,4,5,6,7};
+    assertArrayEquals(new int[]{7,6,5,4,3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+}
